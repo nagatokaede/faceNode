3c140e43b1a43e3bd99f7190c64bcd8491267cf9
nagatokaede <38429526+nagatokaede@users.noreply.github.com>
2018-07-18 11:28:25 +0800

    Update server.js
    // 添加 ajax 跨域解决中间件 koa-cors

--- server/server.js
+++ server/server.js
@@ -7,12 +7,16 @@ const koa = require('koa');
 const Path = require('path');
 const Static = require('koa-static');
 const bodyParser = require('koa-bodyparser');
+const cors = require('koa-cors');
 
 
 const router = require('../router/router');
 const page404 = require('../router/404');
 
 const app = new koa();
+
+// 添加 ajax 跨域解决中间件
+app.use(cors());
 
 // 静态资源目录对于相对入口文件server.js的路径
 const staticPath = ['../testfile', '../static'];
